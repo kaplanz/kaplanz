e48873b82c34ee0c9b9c52615230210cd3cc9cba
Zakhary Kaplan <me@zakhary.dev>
2023-10-25 08:00:00 -0400

    feat: initial

--- README.md
+++ README.md
@@ -14,3 +14,58 @@ Here are some ideas to get you started:
 - 😄 Pronouns: ...
 - ⚡ Fun fact: ...
 -->
+
+<p align="center">
+  <i>
+    I am an engineering graduate, programmer, and aspiring computer architect
+    with a passion for exploring the world of computing.
+  </i>
+</p>
+
+---
+
+[![][github.badge]][github.hyper]
+[![][linkedin.badge]][linkedin.hyper]
+[![][mail.badge]][mail.hyper]
+[![][web.badge]][web.hyper]
+
+### About me
+
+```rust
+use Area::*;
+use Languages::*;
+use Skills::*;
+use Tools::*;
+
+let zakhary = Person {
+  about: Contact {
+    name: "Zakhary Kaplan",
+    email: "me@zakhary.dev",
+    country: "🇨🇦",
+  },
+  edu: vec![Degree {
+    school: "University of Toronto",
+    discipline: "Computer Engineering",
+    level: Level::Bachelors,
+    grad: 2023,
+  }],
+  area: Hardware | Software,
+  work: Developer {
+    languages: vec![Assembly, C, Cpp, Lua, Python, Rust, Verilog],
+    skills: vec![Algorithms, Compilers, Embedded, Emulation, Fpga],
+    tools: vec![Docker, GnuMake, Neovim, Tmux, Unix, Zsh],
+    os: "*nix",
+    editor: "(neo)?vim",
+  },
+};
+```
+
+<!-- Reference-style badges -->
+[github.badge]:   https://img.shields.io/github/followers/kaplanz?label=Follow&style=social
+[github.hyper]:   https://github.com/kaplanz
+[linkedin.badge]: https://img.shields.io/static/v1?label=%20&logo=linkedin&labelColor=555&message=LinkedIn&color=blue
+[linkedin.hyper]: https://linkedin.com/in/zakhary/
+[mail.badge]:     https://img.shields.io/static/v1?label=%20&logo=gmail&labelColor=555&message=Mail&color=red
+[mail.hyper]:     mailto:me@zakhary.dev
+[web.badge]:      https://img.shields.io/static/v1?label=%20&logo=firefoxbrowser&labelColor=555&message=Homepage&color=darkcyan
+[web.hyper]:      https://zakhary.dev
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,117 @@
+use std::ops::BitOr;
+
 fn main() {
-    println!("Hello, world!");
+    use Area::*;
+    use Languages::*;
+    use Skills::*;
+    use Tools::*;
+
+    let zakhary = Person {
+        about: Contact {
+            name: "Zakhary Kaplan",
+            email: "me@zakhary.dev",
+            country: "🇨🇦",
+        },
+        edu: vec![Degree {
+            school: "University of Toronto",
+            discipline: "Computer Engineering",
+            level: Level::Bachelors,
+            grad: 2023,
+        }],
+        area: Hardware | Software,
+        work: Developer {
+            languages: vec![Assembly, C, Cpp, Lua, Python, Rust, Verilog],
+            skills: vec![Algorithms, Compilers, Embedded, Emulation, Fpga],
+            tools: vec![Docker, GnuMake, Neovim, Tmux, Unix, Zsh],
+            os: "*nix",
+            editor: "(neo)?vim",
+        },
+    };
+
+    println!("{:#?};", zakhary);
+}
+
+#[derive(Debug)]
+enum Area {
+    Hardware = 0b01,
+    Software = 0b10,
+}
+
+impl BitOr for Area {
+    type Output = Vec<Self>;
+
+    fn bitor(self, rhs: Self) -> Self::Output {
+        vec![self, rhs]
+    }
+}
+
+#[derive(Debug)]
+enum Languages {
+    Assembly,
+    C,
+    Cpp,
+    Python,
+    Lua,
+    Rust,
+    Verilog,
+}
+
+#[derive(Debug)]
+enum Skills {
+    Algorithms,
+    Compilers,
+    Embedded,
+    Emulation,
+    Fpga,
+}
+
+#[derive(Debug)]
+enum Tools {
+    Docker,
+    GnuMake,
+    Neovim,
+    Tmux,
+    Unix,
+    Zsh,
+}
+
+#[allow(dead_code)]
+#[derive(Debug)]
+struct Person {
+    about: Contact,
+    edu: Vec<Degree>,
+    area: Vec<Area>,
+    work: Developer,
+}
+
+#[allow(dead_code)]
+#[derive(Debug)]
+struct Contact {
+    name: &'static str,
+    email: &'static str,
+    country: &'static str,
+}
+
+#[derive(Debug)]
+enum Level {
+    Bachelors,
+}
+
+#[allow(dead_code)]
+#[derive(Debug)]
+struct Degree {
+    school: &'static str,
+    level: Level,
+    discipline: &'static str,
+    grad: u32,
+}
+
+#[allow(dead_code)]
+#[derive(Debug)]
+struct Developer {
+    languages: Vec<Languages>,
+    skills: Vec<Skills>,
+    tools: Vec<Tools>,
+    os: &'static str,
+    editor: &'static str,
 }
